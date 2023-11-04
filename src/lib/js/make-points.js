efd9e90f402c443d880c6715396fcc31c2b04822
Ahmad Barclay <bothness@users.noreply.github.com>
2023-11-04 22:13:26 +0000

    better random points algorithm

--- src/lib/js/make-points.js
+++ src/lib/js/make-points.js
@@ -1,21 +1,27 @@
-export default function makePoints(count, numCandidates = 5) {
-	let points = [[Math.random(), Math.random()]];
+import { quadtree } from "d3-quadtree";
+
+export default function makePoints(count, numCandidates = 10) {
+	const pt = [Math.random(), Math.random()];
+	const points = [pt];
+	const qt = quadtree().extent([[0, 0], [1, 1]]).add(pt);
+	
 	while (points.length < count) {
-		points.push(sample(points, numCandidates));
+		points.push(sample(qt, numCandidates));
 	}
 	return points;
 }
 
-function sample(samples, numCandidates) {
+function sample(qt, numCandidates) {
   var bestCandidate, bestDistance = 0;
   for (var i = 0; i < numCandidates; ++i) {
     var c = [Math.random(), Math.random()],
-        d = distance(findClosest(samples, c), c);
+        d = distance(qt.find(...c), c);
     if (d > bestDistance) {
       bestDistance = d;
       bestCandidate = c;
     }
   }
+	qt.add(bestCandidate);
   return bestCandidate;
 }
 
@@ -23,12 +29,4 @@ function distance(a, b) {
   var dx = a[0] - b[0],
       dy = a[1] - b[1];
   return Math.sqrt(dx * dx + dy * dy);
-}
-
-function findClosest(samples, point) {
-	let closest = samples[0];
-	for (const s of samples) {
-		if (distance(point, s) < distance(point, closest)) closest = s;
-	}
-	return closest;
 }
